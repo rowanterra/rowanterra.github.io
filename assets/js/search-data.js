c5ba96f381d32252d591445d5dab0dc1568b9bb7
rowanterra <116193188+rowanterra@users.noreply.github.com>
2025-08-12 00:03:47 +0000

    Deploying to gh-pages from @ rowanterra/rowanterra.github.io@08289379ab71377e1b7ba45c2ab13d8aa552de35 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -466,28 +466,35 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%72%6F%77%61%6E.%72.%74%65%72%72%61(%61%74)%67%6D%61%69%6C(%64%6F%74)%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/rowanterra", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/rowan-terra", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0009-0001-3890-705X", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=BSpwNNoAAAAJ&hl", "_blank");
         },
       },{
         id: 'social-custom_social',
