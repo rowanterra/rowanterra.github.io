f18457e554edae85ea4a7d2bc80a14311d230f40
rowanterra <116193188+rowanterra@users.noreply.github.com>
2026-06-09 07:24:30 +0000

    Deploying to gh-pages from @ rowanterra/rowanterra.github.io@8d5117d390f273b8949b6fd7b0e1f35a6a514ece 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "Please find my CV below",
+          description: "Please find my CV below.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
